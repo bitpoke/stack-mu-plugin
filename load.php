5a05b543b00747d54cfdf4f49a33fe931a5b1731
Calin Don <calin.don@gmail.com>
2019-07-10 17:38:29 +0300

    Release v0.0.1

--- load.php
+++ load.php
@@ -3,7 +3,7 @@
  * Plugin Name: Presslabs Stack
  * Plugin URI: http://presslabs.com/stack/
  * Description: Must-Use plugin for Stack
- * Version: git+$Format:%H$
+ * Version: 0.0.1
  * Author: Presslabs
  * Author URI: http://presslabs.com/
  */
