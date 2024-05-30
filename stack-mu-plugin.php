b9651e5dd8f47c3a13bf20b12cfc16c0d1d4828d
Calin Don <calin@bitpoke.cloud>
2024-05-30 13:12:20 +0300

    Use Jetpack Autoloader for handling composer duplicate incompatible dependencies

--- stack-mu-plugin.php
+++ stack-mu-plugin.php
@@ -9,16 +9,16 @@
  */
 
 // we are copied into mu-plugins root
-if ( file_exists( __DIR__ . '/stack-mu-plugin/' . basename( __FILE__ ) ) ) {
-    require_once __DIR__ . '/stack-mu-plugin/' . basename( __FILE__ );
+if (file_exists(__DIR__ . '/stack-mu-plugin/' . basename(__FILE__))) {
+    require_once __DIR__ . '/stack-mu-plugin/' . basename(__FILE__);
 } else {
     // load Composer autoloader if bundled
-    if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-        require_once __DIR__ . '/vendor/autoload.php';
+    if (file_exists(__DIR__ . '/vendor/autoload_packages.php')) {
+        require_once __DIR__ . '/vendor/autoload_packages.php';
     }
 
-    if ( ! class_exists( '\Stack\Config' ) ) {
-        trigger_error( 'Bitpoke Stack WordPress mu-plugin is not fully installed! Please install with Composer or download full release archive.', E_USER_ERROR );
+    if (! class_exists('\Stack\Config')) {
+        trigger_error('Bitpoke Stack WordPress mu-plugin is not fully installed! Please install with Composer or download full release archive.', E_USER_ERROR);
     }
 
     require __DIR__ . '/src/mu-plugin.php';
